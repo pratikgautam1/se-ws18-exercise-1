f596b42b3010cd0bf6229b494dc583be69158977
pratikgautam1 <44314828+pratikgautam1@users.noreply.github.com>
2018-10-28 05:27:54 +0545

    test check

--- TestSuite.java
+++ TestSuite.java
@@ -1,7 +1,4 @@
 import TinyTestJ.Test;
-
-import java.util.Set;
-
 import TinyTestJ.RunTests;
 
 public class TestSuite {
@@ -24,11 +21,18 @@ public class TestSuite {
     Image i = new Image(100,100);
     i.set(99,99,0x123456);
     int len = i.data.length;
-    //assert (false);
-    assert (i.data[29996] == (byte)0x00);
-    assert (i.data[29997] == (byte)0x12);
-    assert (i.data[29998] == (byte)0x34);
-    assert (i.data[29999] == (byte)0x56);  
+    assert (false);
+    /* wrong
+    assert (i.data[3] == (byte)0x12);
+    assert (i.data[2] == (byte)0x34);
+    assert (i.data[1] == (byte)0x56);
+    assert (i.data[0] == (byte)0x00);
+    */
+    
+    assert(i.data[len - 4]==(byte)0x00);
+    assert(i.data[len - 3]==(byte)0x12);
+    assert(i.data[len - 2]==(byte)0x34);
+    assert(i.data[len - 1]==(byte)0x56);
   }
 
   @Test public static void ImageTest4() throws java.io.FileNotFoundException,java.io.IOException {
@@ -40,12 +44,4 @@ public class TestSuite {
     assert (exists);
   }
 
-  @Test public static void ImageTest5() throws java.io.InterruptedIOException,java.io.IOException {
-	  String AddTest = "AdditionalTest.ppm";
-	    Image i = new Image(100,100);
-	    i.writeAddTest(AddTest);    	    
-	    java.io.File f = new java.io.File(AddTest);	    
-	    boolean exists = f.exists() && f.isFile();
-	    assert (exists);  
-  }
 }
